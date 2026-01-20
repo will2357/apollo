e850678a8481b7d2c135fb759156fc4be2263cc5
will2357 <will.highducheck@gmail.com>
2026-01-20 15:03:07 -0500

    Add server

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import db from "./app/models.js";
+import tutorialRoutes from "./app/routes/tutorial.routes.js";
+
+const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the Tutorial Application." });
+});
+
+// Routes
+tutorialRoutes(app);
+
+// Sync database
+db.sequelize.sync().then(() => {
+  console.log("Synced db.");
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
